64b33ab3e6f1940c8a7b57dc2cb4b14c513bf726
github NikolaCzv <nikola.raicic@hotmail.com>
2020-04-23 18:31:20 -0500

    add works

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={{padding: 60}}>
-      <View>
-        <TextInput placeholder="Add Course Goal" style={{borderBottomColor: 'black', borderBottomWidth: 1, padding: 10}}/>
-        <Button title="ADD"/>
-      </View>
-      <View>
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
+  const goalInputHanlder = enteredText => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, {key: Math.random().toString(), value: enteredGoal}]);
+  }
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Add Course Goal"
+        style={styles.inputField}
+        onChangeText={goalInputHanlder}
+        value={enteredGoal}/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
+      <FlatList data={courseGoals} renderItem={itemData => (
+          <View style={styles.listItem}>
+              <Text>{itemData.item.value}</Text>
+          </View>
+      )}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 60
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputField: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 10,
+    width: '80%'
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    margin: 10
+  }
 });
